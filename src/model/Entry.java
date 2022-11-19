9b807e5a7ee0ad047511521974db1e36e82adf59
dheekkshaR <116768851+dheekkshaR@users.noreply.github.com>
2022-11-19 12:25:09 -0500

    entry class

--- src/model/Entry.java
+++ src/model/Entry.java
@@ -1,2 +1,74 @@
-package model;public class Entry {
+package model;
+
+public class Entry {
+  String date;
+  int happiness;
+  int stress;
+  int sleep;
+  int energy;
+  String highlight;
+  public Entry(String date) {
+    this.date=date;
+    happiness=8;
+    stress=0;
+    sleep=8;
+    energy=8;
+    highlight="";
+  }
+  public Entry(String date, int happiness, int stress, int sleep, int energy, String highlight) {
+    this.date=date;
+    this.happiness=happiness;
+    this.stress=stress;
+    this.sleep=sleep;
+    this.energy=energy;
+    this.highlight= highlight;
+  }
+
+  public int getEnergy() {
+    return energy;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public int getHappiness() {
+    return happiness;
+  }
+
+  public int getSleep() {
+    return sleep;
+  }
+
+  public int getStress() {
+    return stress;
+  }
+
+  public String getHighlight() {
+    return highlight;
+  }
+
+  public void setHappiness(int happiness) {
+    this.happiness = happiness;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public void setHighlight(String highlight) {
+    this.highlight = highlight;
+  }
+
+  public void setEnergy(int energy) {
+    this.energy = energy;
+  }
+
+  public void setStress(int stress) {
+    this.stress = stress;
+  }
+
+  public void setSleep(int sleep) {
+    this.sleep = sleep;
+  }
 }
